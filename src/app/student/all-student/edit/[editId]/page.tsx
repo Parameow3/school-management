20c389d44b81d206eefd400c3c4102a15877fe54
Tan Bunchhay <bunchay1280@gmail.com>
2025-01-13 15:51:03 +0700

    update follow deployment error

--- src/app/student/all-student/edit/[editId]/page.tsx
+++ src/app/student/all-student/edit/[editId]/page.tsx
@@ -7,6 +7,7 @@ import Button from "@/components/Button";
 import axios from "axios";
 import { urlToFile } from "../../../../../../util/urlToFile"
 
+
 interface Classroom {
   id: number;
   name: string;
@@ -56,7 +57,7 @@ const Page = () => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
   const [token, setToken] = useState<string | null>(null);
-  const [imagePreview, setImagePreview] = useState<File | string>("");
+  const [imagePreview, setImagePreview] = useState<string | File | undefined>(undefined);
   const [branches, setBranches] = useState<Branch[]>([]);
   const [file, setFile] = useState<File | null>(null);
 
@@ -133,6 +134,24 @@ const Page = () => {
 
     fetchClassrooms();
   }, [token]);
+
+
+
+
+  useEffect(() => {
+    if (imagePreview instanceof File) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setImagePreview(reader.result as string); // Convert File to data URL
+      };
+      reader.readAsDataURL(imagePreview);
+    } else if (typeof imagePreview === "string") {
+      setImagePreview(imagePreview); // Directly use the string URL
+    }
+  }, [imagePreview]);
+
+
+
 
   if (!formData) {
     return <div className="text-center mt-20">Student not found</div>;
@@ -528,7 +547,7 @@ const Page = () => {
 
         <div className="mt-2">
           {imagePreview && (
-            <img
+            <Image
               src={imagePreview} // Show the current or fetched image
               alt="Student"
               className="mt-2 w-40 h-40 object-cover rounded-md border border-gray-300"
